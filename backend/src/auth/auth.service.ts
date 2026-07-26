bc634d77db370c5c4691e563191f0552cabcb730
vux47 <24106540@st.phenikaa-uni.edu.vn>
2026-07-26 04:21:30 +0000

    feat(auth): implement register API

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+} from '@nestjs/common';
+
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from '../users/users.service';
+
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  constructor(
+    private readonly usersService: UsersService,
+  ) {}
+
+  async register(registerDto: RegisterDto) {
+
+    const user = await this.usersService.findByEmail(
+      registerDto.email,
+    );
+
+    if (user) {
+
+      throw new BadRequestException(
+        'Email đã tồn tại',
+      );
+
+    }
+
+    const password = await bcrypt.hash(
+      registerDto.password,
+      10,
+    );
+
+    await this.usersService.create({
+
+      fullName: registerDto.fullName,
+
+      email: registerDto.email,
+
+      password,
+
+    });
+
+    return {
+
+      message: 'Đăng ký thành công',
+
+    };
+
+  }
+
+}
